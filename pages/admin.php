cc85ce389657beb0dce2490bf46ebf678ad21434
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-17 09:46:59 +0100

    addin admin panel

---
+++ pages/admin.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TrainingTech - User</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="../styles/styles.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="user.php">
+            <img src="../assets/img/LOGO.png" alt="Logo" width="30" height="30" class="d-inline-block align-text-top">
+            Training-Tech
+            </a>
+            <div class="text-center">
+                <?php
+                    echo "<h6>ADMIN</h6>";
+                ?>
+                <a href="../index.php" class="link-dark"><button class="btn btn-sm btn-outline-secondary">LOG OUT</button></a>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid bg-dark text-white">
+            <div class="container p-3" id="cartTable">
+                <h1>ADMIN PANEL</h1>
+            </div>
+    </div>
+    <footer id="sticky-footer" class="flex-shrink-0 py-4">
+    <div class="container text-center">
+        <small>Copyright &copy; Training-Tech</small>
+    </div>
+    </footer>
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
