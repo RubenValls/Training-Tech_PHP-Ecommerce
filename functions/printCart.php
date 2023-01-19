3525e507f2c7b85adfecdb0372240466ac0e6eee
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-19 13:47:14 +0100

    cart printed correctly

--- functions/printCart.php
+++ functions/printCart.php
@@ -13,17 +13,9 @@
 
         if ($result->num_rows > 0) {
             // output data of each row
-            while($row = $result->fetch_assoc()) {
-                var_dump($row);
-            }
-        }
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-            print '<table class="table">';
+            print '<table class="table table-hover table-light table-bordered text-center">';
             print '<thead>
-                        <tr>
-                            <th scope="col">ID Product</th>
+                        <tr class="table-secondary">
                             <th scope="col">Name</th>
                             <th scope="col">Price</th>
                             <th scope="col">Order</th>
@@ -31,15 +23,25 @@
                         </tr>
                     </thead>';
             print '<tbody>';
+            $toPay = 0;
             while($row = $result->fetch_assoc()) {
                 print '<tr>';
-                print '<th scope="row">'. $row["ID"] .'</th>';
-                print '<td>'. $row["NAME"] .'</td>';
+                print '<td>'. $row["nameProduct"] .'</td>';
                 print '<td>'. $row["nameProduct"] .'</td>';
                 print '<td>'. $row["quantity"] .'</td>';
                 print '<td>'. $row["total_price"] .' $</td>';
                 print '</tr>';
+                $toPay += $row["total_price"];
             }
+            print '</tbody>';
+            print '</table>';
+            print '<div class="container">
+                        <h3>Total: '. $toPay .' $</h3>
+                    </div>';
+            print '<div class="container">
+                        <button type="button" class="btn btn-success">Pay Cart</button>
+                        <button type="button" class="btn btn-danger">Delete Cart</button>
+                    </div>';
         }
 
         $db->close();
