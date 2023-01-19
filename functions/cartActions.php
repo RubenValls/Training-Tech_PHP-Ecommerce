7e2d9a7d35b29ce814f030ffb93cd6df4b7bc6bc
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-19 15:18:21 +0100

    Implementing PayCart action

--- functions/cartActions.php
+++ functions/cartActions.php
@@ -14,6 +14,32 @@ function deleteCart(){
     }
 
     function payCart(){
-        print '<h1>PAYED</h1>';
+        $db = mysqli_connect('localhost', 'root', '', 'formatech');
+        if ($db->connect_error) {
+            die("Connection failed: " . $db->connect_error);
+        }
+
+        /*
+        $user = $_SESSION['email'];
+        $sql = "SELECT * FROM cart_products WHERE email = '$user'";
+        $resultCart = $db->query($sql);
+        */
+
+        $sql = "SELECT NAME,QUANTITY FROM products";
+        $resultStock = $db->query($sql);
+        $arrayStock = array();
+
+        if ($resultStock->num_rows > 0) {
+            while($row = $resultStock->fetch_assoc()){
+                array_push($arrayStock, $row);
+            }
+        }
+
+        $db->close();
+
+        foreach($arrayStock as $product){
+            print '<h1>'.$product['NAME'].'</h1>';
+            print '<h1>'.$product['QUANTITY'].'</h1>';
+        }
     }
 ?>
